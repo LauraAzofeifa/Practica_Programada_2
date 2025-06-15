40e3f23088fc6687f06612da431d2ea6d3dd788b
Harlyn Luna <harlynlunab@gmail.com>
2025-06-15 10:24:23 -0600

    Desarrollo del modulo Car
    Se desarrolla el modulo de "Car" de la practica programada.

---
+++ ProgramacionAvanzada.Web/Controllers/CarController.cs
@@ -0,0 +1,63 @@
+﻿using ProgramacionAvanzada.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProgramacionAvanzada.Web.Controllers
+{
+    public class CarController : Controller
+    {
+        // Lista estática en memoria
+        private static List<Car> cars = new List<Car>
+        {
+            new Car { Id = "1", brand = "Toyota", Description = "Sprinter Trueno AE86", DateCreated = DateTime.Now },
+            new Car { Id = "2", brand = "Mazda", Description = "RX-7 FD3S", DateCreated = DateTime.Now },
+            new Car { Id = "3", brand = "Mazda", Description = "RX-7 FC3S", DateCreated = DateTime.Now },
+            new Car { Id = "4", brand = "Nissan", Description = "Silvia S13", DateCreated = DateTime.Now },
+            new Car { Id = "5", brand = "Nissan", Description = "Skyline GT-R R32", DateCreated = DateTime.Now },
+            new Car { Id = "6", brand = "Subaru", Description = "Impreza WRX STi", DateCreated = DateTime.Now },
+            new Car { Id = "7", brand = "Honda", Description = "Civic EG6", DateCreated = DateTime.Now },
+            new Car { Id = "8", brand = "Toyota", Description = "MR2 SW20", DateCreated = DateTime.Now },
+            new Car { Id = "9", brand = "Mitsubishi", Description = "Lancer Evolution III", DateCreated = DateTime.Now },
+            new Car { Id = "10", brand = "Nissan", Description = "180SX", DateCreated = DateTime.Now },
+
+        };
+
+        // Metodo GET - Listar vehiculos
+        public ActionResult Index()
+        {
+            return View(cars);
+        }
+
+        // Metodo GET - Crear un vehiculo
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // Metodo POST - Crear un vehiculo
+        [HttpPost]
+        public ActionResult Create(Car car)
+        {
+            car.Id = (cars.Count + 1).ToString();
+            car.DateCreated = DateTime.Now;
+            cars.Add(car);
+            return View("Index", cars);
+        }
+
+        // Metodo POST - Eliminar un vehiculo
+        [HttpPost]
+        public ActionResult Delete(string id)
+        {
+            var car = cars.FirstOrDefault(c => c.Id == id);
+            if (car != null)
+            {
+                cars.Remove(car);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ProgramacionAvanzada.Web/Models/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProgramacionAvanzada.Web.Models
+{
+    public class Car
+    {
+        // Id del vehuculo
+        public string Id { get; set; }
+
+        // Marca del vehiculo
+        public string brand { get; set; }
+
+        // Descripción del vehiculo
+        public string Description { get; set; }
+
+        // Fecha de creación del vehiculo
+        public DateTime DateCreated { get; set; }
+    }
+}
